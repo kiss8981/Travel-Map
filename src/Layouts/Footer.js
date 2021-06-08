1d324c106a5ae99e33b68088d5367f6ca819f27e
kiss8981 <kdh89811@gmail.com>
2021-06-08 09:09:23 +0900

    [FIX] 푸터 이미지 수정

--- src/Layouts/Footer.js
+++ src/Layouts/Footer.js
@@ -12,8 +12,8 @@ const Header = () => {
             <Link className="nav-link footer-link" to="/list">목록</Link>
             <Link className="nav-link footer-link" to="/add">기록하기</Link>
             <ul className="footer-social">
-                <li><a href="https://github.com/kiss8981"><img src={Github_logo} alt="github" className="github_icon"/></a></li>
-                <li><a href="https://kiss8981.github.io/"><img src={Rss_logo} alt="blog" className="blog_icon"/></a></li>
+                <li><a href="https://github.com/kiss8981"><img src='https://travel.audiscordbot.xyz/image/github_logo.svg' alt="github" className="github_icon"/></a></li>
+                <li><a href="https://kiss8981.github.io/"><img src='https://travel.audiscordbot.xyz/image/rss_logo.svg' alt="blog" className="blog_icon"/></a></li>
             </ul>
             <div className="footer-text">Copyright © 2021 Dohyun Kim.<br/>All rights reserved</div>
         </footer>

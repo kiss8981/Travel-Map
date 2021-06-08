07dcb98b5ae3c956f84bfd07d4ae2151f7f39c01
kiss8981 <kdh89811@gmail.com>
2021-06-08 21:53:53 +0900

    [FIX] 지도 클러스터 할 최소 지도 레벨 변경

--- src/utills/map.js
+++ src/utills/map.js
@@ -45,7 +45,7 @@ export default function Map() {
     var clusterer = new kakao.maps.MarkerClusterer({
       map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
       averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
-      minLevel: 9 // 클러스터 할 최소 지도 레벨 
+      minLevel: 11 // 클러스터 할 최소 지도 레벨 
     });
     var makers = []
     for (var i=0; i< data.length; i++) {

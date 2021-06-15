16979a6f0cc26cbb10a505375f9bff9b25c0b6f4
kiss8981 <kdh89811@gmail.com>
2021-06-15 21:13:52 +0800

    [FIX] 빌드설정 변경

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = process.env.PORT || 8080
+app.listen(port);
+
+app.use(express.static(path.join(__dirname, '/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/build/index.html'));
+});
+
+console.log(`server running ${port}`)

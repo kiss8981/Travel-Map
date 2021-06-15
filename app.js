c524c0a0ea1ff7a65e03dd67149f6440e2cc4078
kiss8981 <kdh89811@gmail.com>
2021-06-15 22:48:46 +0800

    [FIX] 사이트맵

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.listen(port);
 
 app.use(express.static(path.join(__dirname, '/build')));
 
+app.get('/privacy', (req, res) => {
+    res.sendFile(path.join(__dirname+'/build/privacy.html'));
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/build/index.html'));
 });
--- src/Layouts/Footer.js
+++ src/Layouts/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
             <Link className="nav-link footer-link" to="/list">목록</Link>
             <Link className="nav-link footer-link" to="/add">기록하기</Link>
             <Link className="nav-link footer-link" to="/profile">프로필</Link>
-            <Link className="nav-link footer-link" href="https://github.com/kiss8981/Travel-Map/blob/main/%EA%B0%9C%EC%9D%B8%EC%A0%95%EB%B3%B4%EC%B2%98%EB%A6%AC%EB%B0%A9%EC%B9%A8.md">개인정보처리방침</Link>
+            <a className="nav-link footer-link" href="https://travel-report.xyz/privacy">개인정보처리방침</a>
             </ul>
             <div className="footer-text">Copyright 2021 Dohyun Kim.<br/>All rights reserved</div>
         </footer>
